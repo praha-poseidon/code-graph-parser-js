845af5ccfcf959c9bc29c5b238c47b8be012ba09
praha-poseidon <xiyangyulei@sina.cn>
2026-08-01 13:16:56 +0800

    Update node-id.ts

--- src/parser/node-id.ts
+++ src/parser/node-id.ts
@@ -2,6 +2,11 @@ export function moduleId(projectName: string, projectFilePath: string): string {
   return `${projectName}#${projectFilePath}`;
 }
 
+/** Directory package id. packagePath is project-relative, e.g. src/components/Foo */
+export function directoryPackageId(projectName: string, packagePath: string): string {
+  return `${projectName}#pkg#${packagePath}`;
+}
+
 export function unitId(projectName: string, projectFilePath: string, name: string): string {
   return `${projectName}#${projectFilePath}::${name}`;
 }
